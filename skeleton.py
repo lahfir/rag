deb0e0e1f838f4db09c26a3492606a409ed088ac
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2023-12-11 16:41:32 -0600

    save point

--- skeleton.py
+++ skeleton.py
@@ -1,11 +1,63 @@
+import os
+import weaviate
+from dotenv import load_dotenv
+from llama_index import SimpleDirectoryReader
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.vector_stores import WeaviateVectorStore
+from llama_index import VectorStoreIndex, StorageContext
 from llama_index import SimpleDirectoryReader, VectorStoreIndex, ChatMemoryBuffer
 import openai
 
 class RAGChatbot:
-    def __init__(self, directory, openai_api_key):
+    def __init__(self, directory,openai_api_key, \
+                 weaviate_uri="http://localhost:8080", \
+                 system_prompt=None \
+                 
+                 ):
+        self.key = os.getenv("OPENAI_API_KEY")
+        self.weaviate_uri = weaviate_uri
+
         self.directory = directory
         # Set up OpenAI API key
-        openai.api_key = openai_api_key
+        openai.api_key = self.key
+
+    
+    def connect_to_weaviate_client(self): 
+        key = os.getenv("OPENAI_API_KEY")
+
+        # load the blogs using the reader
+        docs = SimpleDirectoryReader('./files').load_data()
+
+        # chunk up the blog posts into nodes
+        parser = SimpleNodeParser.from_defaults(chunk_size=1024, chunk_overlap=20)
+        nodes = parser.get_nodes_from_documents(docs)
+
+        try:
+            client = weaviate.Client(url=self.weaviate_uri)
+            print("Connected to Weaviate")
+            return client
+        except Exception as e:
+            print(f"Failed to connect to Weaviate: {str(e)}")
+            return
+
+    def get_query_engine(self, client)
+
+        try:
+            # construct vector store
+            vector_store = WeaviateVectorStore(weaviate_client=client, index_name="DataIndex", text_key="content")
+
+            # setting up the storage for the embeddings
+            storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+            # set up the index
+            index = VectorStoreIndex(nodes, storage_context=storage_context)
+
+            query_engine = index.as_query_engine()
+            return query_engine
+        except Exception as e:
+            print(str(e))
+
+
 
     def connect_to_llama_index(self):
         # Connect to LlamaIndex and create a vector store index
